18679f31c12eeaf4e63f01e48f645e2986612dff
Aida7 <33822250+Aida7@users.noreply.github.com>
2018-04-22 19:31:19 +0600

    codeFirst

---
+++ CodeFirst/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class Context:DbContext
+    {
+        public Context() : base("ConnectString")
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ CodeFirst/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    
+        public class User
+        {
+            public int Id { get; set; }
+            public string Sername { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public int Age { get; set; }
+            
+            public virtual List<Order> Orders { get; set; }
+        }
+
+        public class Order
+        {
+            public int OrderId { get; set; }
+            public string ProductName { get; set; }
+            public int Count { get; set; }
+            public DateTime PurchaseDate { get; set; }
+
+            public User User { get; set; }
+        }
+    }
+
